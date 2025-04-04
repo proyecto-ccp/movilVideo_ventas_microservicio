ea794596b4fc42b5e5a2d52ae41f402f8c10075b
Eduar Romero <eg.romeror1@uniandes.edu.co>
2025-04-03 22:48:02 -0500

    Versión inicial microservicio cargar video

--- ProcesarVideo.Aplicacion/Dto/VideoIn.cs
+++ ProcesarVideo.Aplicacion/Dto/VideoIn.cs
@@ -8,10 +8,11 @@ namespace Videos.Aplicacion.Dto
 {
     public class VideoIn
     {
-        public Guid Id { get; set; }
+        //public Guid Id { get; set; }
         public Guid IdCliente { get; set; }
         public int IdProducto { get; set; }
         public string Nombre { get; set; }
+        public string Video { get; set; }
         public string UrlVideo { get; set; }
         public string UrlImagen { get; set; }
         public string EstadoCarga { get; set; }
