f65d3fb7c116388281044688b0831b9a821bdf77
Eduar Romero <eg.romeror1@uniandes.edu.co>
2025-04-06 12:15:12 -0500

    se retira cuenta de servicio y se modifica Dockerfile

--- ProcesarVideo.Aplicacion/obj/Debug/net8.0/Videos.Aplicacion.AssemblyInfo.cs
+++ ProcesarVideo.Aplicacion/obj/Debug/net8.0/Videos.Aplicacion.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Videos.Aplicacion")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a238fd0d879ea7f23f8305b611469e5f2bc5f2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c41eecf3a6c4a4a01b7e81c13f0a4f6653dfa86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Videos.Aplicacion")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Videos.Aplicacion")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
