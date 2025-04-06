e892f232bad0c8c54483fe29f435b4c1107a459c
Eduar Romero <eg.romeror1@uniandes.edu.co>
2025-04-05 22:03:51 -0500

    Inclusión plataforma de mensajeria

--- ProcesarVideo.Aplicacion/Mapeadores/VideoMapeador.cs
+++ ProcesarVideo.Aplicacion/Mapeadores/VideoMapeador.cs
@@ -25,9 +25,9 @@ namespace Videos.Aplicacion.Mapeadores
                 .ForMember(dest => dest.Nombre, opt => opt.MapFrom(src => src.Nombre))
                 .ForMember(dest => dest.Video, opt => opt.MapFrom(src => src.Archivo))
                 //.ForMember(dest => dest.Ruta, opt => opt.MapFrom(src => src.Ruta))
-                .ForMember(dest => dest.UrlVideo, opt => opt.MapFrom(src => src.UrlVideo))
-                .ForMember(dest => dest.UrlImagen, opt => opt.MapFrom(src => src.UrlImagen))
-                .ForMember(dest => dest.EstadoCarga, opt => opt.MapFrom(src => src.EstadoCarga))
+                //.ForMember(dest => dest.UrlVideo, opt => opt.MapFrom(src => src.UrlVideo))
+                //.ForMember(dest => dest.UrlImagen, opt => opt.MapFrom(src => src.UrlImagen))
+                //.ForMember(dest => dest.EstadoCarga, opt => opt.MapFrom(src => src.EstadoCarga))
                 .ReverseMap();
 
             CreateMap<VideoOut,VideoIn>()
@@ -35,9 +35,9 @@ namespace Videos.Aplicacion.Mapeadores
                 .ForMember(dest => dest.IdCliente, opt => opt.MapFrom(src => src.Video.IdCliente))
                 .ForMember(dest => dest.IdProducto, opt => opt.MapFrom(src => src.Video.IdProducto))
                 .ForMember(dest => dest.Nombre, opt => opt.MapFrom(src => src.Video.Nombre))
-                .ForMember(dest => dest.UrlVideo, opt => opt.MapFrom(src => src.Video.UrlVideo))
-                .ForMember(dest => dest.UrlImagen, opt => opt.MapFrom(src => src.Video.UrlImagen))
-                .ForMember(dest => dest.EstadoCarga, opt => opt.MapFrom(src => src.Video.EstadoCarga))
+                //.ForMember(dest => dest.UrlVideo, opt => opt.MapFrom(src => src.Video.UrlVideo))
+                //.ForMember(dest => dest.UrlImagen, opt => opt.MapFrom(src => src.Video.UrlImagen))
+                //.ForMember(dest => dest.EstadoCarga, opt => opt.MapFrom(src => src.Video.EstadoCarga))
                 .ReverseMap();
         }
     }
