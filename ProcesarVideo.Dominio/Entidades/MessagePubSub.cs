b09d0088afb3b6f7c9509716c5ee33f44e2c8f1c
Eduar Romero <eg.romeror1@uniandes.edu.co>
2025-04-05 21:55:19 -0500

    Publicación de mensaje en plataforma

---
+++ ProcesarVideo.Dominio/Entidades/MessagePubSub.cs
@@ -0,0 +1,9 @@
+﻿namespace Videos.Dominio.Entidades
+{
+    public class MessagePubSub
+    {
+        public Guid Id { get; set; }
+        public string Nombre { get; set; }
+        public string Archivo { get; set; }
+    }
+}
--- ProcesarVideo.Dominio/Servicios/CargarVideo.cs
+++ ProcesarVideo.Dominio/Servicios/CargarVideo.cs
@@ -1,16 +1,17 @@
 ﻿using Videos.Dominio.Entidades;
 using Videos.Dominio.Puertos.Repositorios;
-using Google.Apis.Storage.v1.Data;
 using Google.Cloud.Storage.V1;
-using Microsoft.VisualBasic;
+using Google.Cloud.PubSub.V1;
 using System.Text;
 using Google.Apis.Auth.OAuth2;
+using System.Text.Json;
 
 namespace Videos.Dominio.Servicios
 {
     public class CargarVideo(IVideoRepositorio videoRepositorio)
     {
         private readonly IVideoRepositorio _videoRepositorio = videoRepositorio;
+        GoogleCredential credential = null;
         public async Task Cargar(Video video)
         {
             if (ValidarVideo(video))
@@ -18,28 +19,11 @@ namespace Videos.Dominio.Servicios
                 video.Id = Guid.NewGuid();
                 video.FechaCreacion = DateTime.Now;
                 video.UrlVideo = "https://storage.googleapis.com/videos_ccp/" + video.Nombre;
+                video.EstadoCarga = "Cargado";
+
                 await videoRepositorio.Cargar(video);
-
-                GoogleCredential credential = null;
-                using (var jsonStream = new FileStream("../../Recursos/experimento-ccp-8172d4037e96.json", FileMode.Open,
-                    FileAccess.Read, FileShare.Read))
-                {
-                    credential = GoogleCredential.FromStream(jsonStream);
-                }
-
-                var gcsStorage = StorageClient.Create(credential);
-                var file = Encoding.UTF8.GetBytes(video.Archivo);
-
-                await gcsStorage.UploadObjectAsync(
-                        "videos_ccp",
-                        video.Nombre,
-                        "video/mp4",
-                        new MemoryStream(file),
-                        new UploadObjectOptions
-                        {
-                            PredefinedAcl = PredefinedObjectAcl.PublicRead
-                        });
-                
+                await AlmacenarVideo(video);
+                PublicarMensaje(video);
             }
             else
             {
@@ -47,9 +31,55 @@ namespace Videos.Dominio.Servicios
             }
         }
 
+        private void PublicarMensaje(Video video)
+        {
+            using (var jsonStream = new FileStream("../../Recursos/experimento-ccp-8172d4037e96.json", FileMode.Open,
+                FileAccess.Read, FileShare.Read))
+            {
+                credential = GoogleCredential.FromStream(jsonStream);
+            }
+
+            MessagePubSub mensajePubSub = new MessagePubSub();
+            mensajePubSub.Id = video.Id;
+            mensajePubSub.Archivo = video.Archivo;
+            mensajePubSub.Nombre = video.Nombre;
+            var topicName = new TopicName("experimento-ccp", "video-ccp");
+            var publisher = PublisherServiceApiClient.Create();
+
+            var message = System.Text.Encoding.UTF8.GetBytes(JsonSerializer.Serialize(mensajePubSub));
+            var pubsubMessage = new PubsubMessage
+            {
+                Data = Google.Protobuf.ByteString.CopyFrom(message)
+            };
+            publisher.Publish(topicName, new[] { pubsubMessage });
+        }
+
+        private async Task AlmacenarVideo(Video video)
+        {
+            //using (var jsonStream = new FileStream("../../Recursos/experimento-ccp-8172d4037e96.json", FileMode.Open,
+            //    FileAccess.Read, FileShare.Read))
+            //{
+            //    credential = GoogleCredential.FromStream(jsonStream);
+            //}
+
+            var gcsStorage = StorageClient.Create(credential);
+            byte[] binaryData = Convert.FromBase64String(video.Archivo);
+            var file = System.Text.Encoding.UTF8.GetBytes(video.Archivo);
+
+            await gcsStorage.UploadObjectAsync(
+                    "videos_ccp",
+                    video.Nombre,
+                    "video/mp4",
+                    new MemoryStream(binaryData),
+                    new UploadObjectOptions
+                    {
+                        PredefinedAcl = PredefinedObjectAcl.PublicRead
+                    });
+        }
+
         public bool ValidarVideo(Video video)
         {
-            return video.IdCliente != Guid.Empty && video.IdProducto != 0 && !string.IsNullOrEmpty(video.Nombre) && !string.IsNullOrEmpty(video.UrlVideo) && !string.IsNullOrEmpty(video.Archivo);
+            return video.IdCliente != Guid.Empty && video.IdProducto != 0 && !string.IsNullOrEmpty(video.Nombre) && !string.IsNullOrEmpty(video.Archivo);
         }
     }
 }
